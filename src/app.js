7dcdada51f83f535a8dd56ad1982c99d4378b60f
MaeganAlexander <126523053+MaeganAlexander@users.noreply.github.com>
2023-04-18 20:08:39 +0100

    adding to the js to make weather app operable.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 function displayTemperature(response) {
-  console.log(respinse.data);
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.inneHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 let apiKey = "5fd472b7acba333cd8a035ea85a0d4d4c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
