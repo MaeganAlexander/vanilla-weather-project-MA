bcfcd52a086fb8e090a799f68286e6e942b68ca6
MaeganAlexander <126523053+MaeganAlexander@users.noreply.github.com>
2023-04-18 21:35:32 +0100

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -3,9 +3,13 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.inneHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHtml = Math.round(response.data.wind.speed);
 }
 let apiKey = "5fd472b7acba333cd8a035ea85a0d4d4c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
