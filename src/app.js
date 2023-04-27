48a79403f691ca79aa4a1fa235019142703bdb4b
MaeganAlexander <126523053+MaeganAlexander@users.noreply.github.com>
2023-04-27 18:43:31 +0100

    change to app.js from advice from shecodes slack

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// change here
 function formateDate(timestamp) {
   let date = newDate(timestamp);
   let hours = date.getHours();
@@ -48,20 +49,16 @@ function displayTemperature(response) {
   getForecast(response.data.coord);
 }
 function search(city) {
-  let apiKey = "5fd472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
-g=London&appid=$(appKey)&units=metric`;
+  let apiKey = "cd173a006b0e51dac58c6d8064c94178";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 function handleSubmit(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector("#city-Input");
+  let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
   console.log(cityInputElement.value); //may remove later date
 }
 
-console.log(apiUrl); // can remove later
-axios.get(apiurl).then(displayTemperature);
-
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", handlesSubmit);
+form.addEventListener("submit", handleSubmit);
